c41f50f75f296471d6c961b2f2bd8bfdd7e87856
Odierna Thomas <odiernathomas@gmail.com>
2026-08-30 17:27:54 +0200

    aggiunta dei cookie per salvare le valutazioni e creazione di una tabella dove verrano poi inserite

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function setCookie(nome, valore, giorni) {
+  let scadenza = "";
+  if (giorni) {
+    const d = new Date();
+    d.setTime(d.getTime() + (giorni * 24 * 60 * 60 * 1000));
+    scadenza = "; expires=" + d.toUTCString();
+  }
+  document.cookie = nome + "=" + encodeURIComponent(valore) + scadenza + "; path=/; SameSite=Lax";
+}
+
+function getCookie(nome) {
+  const nameEQ = nome + "=";
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) {
+      return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+  }
+  return null;
+}
+
+function aggiungiVoto(materia, voto) {
+  let votiAttuali = getCookie("votiScolastici") || "";
+  
+  if (votiAttuali !== "") {
+    votiAttuali += ",";
+  }
+  
+  votiAttuali += materia + ":" + voto;
+  setCookie("votiScolastici", votiAttuali, 30);
+}
+
+function ottieniListaVoti() {
+  const votiStringa = getCookie("votiScolastici");
+  if (!votiStringa) return [];
+  return votiStringa.split(",");
+}
+
+
 const btnSwitch = document.getElementById('btn-switch');
 const sezioneOrario = document.getElementById('sezione-orario');
 const sezioneVoti = document.getElementById('sezione-voti');
@@ -21,3 +62,22 @@ btnSwitch.addEventListener('click', () => {
     btnSwitch.innerHTML = '<span class="icon-text">Voti</span> ';
   }
 });
+
+const formVoto = document.getElementById('form-voto');
+
+if (formVoto) {
+  formVoto.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const materia = document.getElementById('materia-select').value;
+    const voto = document.getElementById('voto-input').value;
+
+    // Salva il voto nel cookie
+    aggiungiVoto(materia, voto);
+
+    // Stampa in console per verificare i dati salvati
+    console.log("Voti attualmente nei cookie:", ottieniListaVoti());
+
+    formVoto.reset();
+  });
+}
